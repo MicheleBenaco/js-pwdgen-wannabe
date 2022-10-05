17e213c996415f4c05d45f4964f0c979a79b3e89
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-05 15:55:46 +0200

    inizio

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// poi chiedi la sua età
+// Infine scrivi sulla pagina nomecognomecolorepreferito21 
+// (dove il 21 è in questo caso l'età)
+
+// costante nomeCostante/variabile = comando('');
+const NomeUtente = prompt('Come ti chiami?');
+const CognomeUtente = prompt('Qell e il tuo Cognome?');
+const ColorePreUtente = prompt('Quall e il tuo colore preferito ?');
+const EtaUtente = prompt('Quanti anni hai ?');
