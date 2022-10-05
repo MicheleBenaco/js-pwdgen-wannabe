f2abc5ffa3685277497c0567d95ac13d2d422e02
MicheleBenaco <benaco.michele@yahoo.it>
2022-10-05 16:19:04 +0200

    js

--- script/script.js
+++ script/script.js
@@ -10,3 +10,12 @@ const NomeUtente = prompt('Come ti chiami?');
 const CognomeUtente = prompt('Qell e il tuo Cognome?');
 const ColorePreUtente = prompt('Quall e il tuo colore preferito ?');
 const EtaUtente = prompt('Quanti anni hai ?');
+const saluto = `
+
+    ${NomeUtente} ${CognomeUtente} ${ColorePreUtente} ${EtaUtente}
+`;
+
+// const elemento = Funziona anche senza questa variabile
+ document.getElementById('id_benvenuto').innerHTML = saluto;
+
+// elemento.innerHTML = saluto;
